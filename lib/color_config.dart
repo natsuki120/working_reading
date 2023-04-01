c75f86ff8b8288c0177be0635ff8d69fa1a8852d
natsuki <synchro0718@icloud.com>
2023-04-01 12:48:28 +0900

    add: color_config.dart

---
+++ lib/color_config.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+
+// Main Color
+
+const Color primary = Color(0xff607D8B);
+
+const Color secondary = Color(0xff8b6e60);
+
+const Color white = Color(0xffFFFFFF);
+
+const Color black = Color(0xff000000);
+
+// Background
+
+const Color backgroundWhite = Color(0xffFF4F4);
+
+const Color backgroundBlack = Color(0xff111414);
+
+// Surface Color
+
+const Color surfacePrimary = Color(0xffFFFFFF);
+
+const Color surfaceSecondary = Color(0xff12181A0D);
+
+const Color surfaceTertiary = Color(0xff12181A1A);
+
+const Color surfaceDisabled = Color(0xff12181A33);
+
+const Color onSurface = Color(0xff201A19);
+
+// Text&Icon
+
+const Color highEmphasis = Color(0xff111414);
+
+Color midEmphasis = Color(0xff12181A).withOpacity(0.7);
+
+Color lowEmphasis = Color(0xff12181A).withOpacity(0.5);
+
+const Color disabledOnlyText = Color(0xff12181A4D);
+
+const Color accentPrimary = Color(0xff2B5B64);
+
+const Color accentSecondary = Color(0xffC87149);
+
+const Color link = Color(0xff0071CC);
+
+const Color disabled = Color(0xff12181A66);
+
+const Color onPrimary = Color(0xffffffff);
+
+// Border
+
+const Color borderHighEmphasis = Color(0xff12181A99);
+
+const Color borderMidEmphasis = Color(0xff12181A66);
+
+const Color borderLowEmphasis = Color(0xff12181A33);
+
+const Color borderDisabled = Color(0xff12181A40);
