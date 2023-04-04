06a300db2cfb8ee06635ceef77907ba0eab583ba
natsuki <synchro0718@icloud.com>
2023-04-04 10:38:06 +0900

    add: secondary_color_button.dart

---
+++ lib/component/secondary_color_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../color_config.dart';
+import '../font_config.dart';
+
+class SecondaryColorButton extends StatelessWidget {
+  const SecondaryColorButton({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(secondary),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: bodyBold(whitePrimary),
+        ),
+      ),
+    );
+  }
+}
