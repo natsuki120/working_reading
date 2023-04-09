13e6c538146e4d63e4128833a2486d000d1d6cbd
natsuki <synchro0718@icloud.com>
2023-04-09 17:27:55 +0900

    update: answer_page.dart
    正解したら？？から答えを出すようにした
    回答するためにテキストフィールドを設置した
    回答しないと採点ボタンが押せないようにした
    問題の設置場所をより真ん中よりにした

--- lib/page/answer_page.dart
+++ lib/page/answer_page.dart
@@ -1,87 +1,167 @@
 import 'package:flutter/material.dart';
-import 'package:working_reading/page/result_page.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:working_reading/component/disable_button.dart';
+import 'package:working_reading/component/primary_color_button.dart';
+import 'package:working_reading/domain/sentence/sentence_notifier.dart';
+import 'package:working_reading/domain/sentence_list/sentence_list.dart';
 import '../color_config.dart';
+import '../domain/sentence/sentence.dart';
 import '../font_config.dart';
 
-class AnswerPage extends StatelessWidget {
+class AnswerPage extends HookConsumerWidget {
   const AnswerPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useTextEditingController(text: '');
+    SentenceList sentenceListNotifier = ref.watch(sentenceListNotifierProvider);
+
+    final _areFieldsEmpty =
+        useState<bool>(true); // controll the button based on Text.isEmpty
+
+    bool areFieldsEmpty() {
+      return controller.text.toString().isEmpty;
+    }
+
+    useEffect(() {
+      controller.addListener(() {
+        _areFieldsEmpty.value = areFieldsEmpty();
+      });
+      return null;
+    }, [controller]);
+
+    String qText = '';
+
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(backgroundColor: backgroundColor),
-      body: Column(
-        children: [
-          // TODO 左に偏ってるからなんとかする
-          Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
-            for (int i = 1; i <= 5; i++)
-              Wrap(
-                children: [
-                  Column(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            // TODO 左に偏ってるからなんとかする
+            // 文字数が違うから？？？？の時と答えが出ている時で改行が変わる
+            Wrap(
+              children: [
+                for (int i = 1; i <= 5; i++)
+                  Wrap(
                     children: [
-                      Wrap(
+                      Column(
                         children: [
-                          Text(
-                            '問1. ',
-                            style: title1Regular(blackSecondary),
+                          Wrap(
+                            children: [
+                              Text(
+                                '問$i. ',
+                                style: title1Regular(blackSecondary),
+                              ),
+                              Text(
+                                sentenceListNotifier
+                                        .sentenceList[i - 1].hasCollected
+                                    ? sentenceListNotifier
+                                        .sentenceList[i - 1].properNoun
+                                    : '？？',
+                                style: title1Regular(blackSecondary),
+                              ),
+                            ],
                           ),
-                          Text(
-                            '？？？？',
-                            style: title1Regular(blackSecondary),
-                          ),
+                          const SizedBox(height: 64),
                         ],
                       ),
-                      const SizedBox(height: 64),
+                      const SizedBox(width: 32),
                     ],
                   ),
-                  const SizedBox(width: 16),
-                ],
+              ],
+            ),
+            const Spacer(),
+            // 回答用のテキストフィールド
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: '回答を入力してください',
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: secondary,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
-          ]),
-          // ボタン
-          const Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 150,
-                height: 64,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(
-                      side: BorderSide(color: primary),
+            ),
+            const SizedBox(height: 32),
+            // ボタン
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  height: 64,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: primary),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'ギブアップ',
+                      style: bodyBold(primary),
                     ),
                   ),
-                  onPressed: () {},
-                  child: Text(
-                    'ギブアップ',
-                    style: bodyBold(primary),
-                  ),
                 ),
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              SizedBox(
-                width: 150,
-                height: 64,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(primary),
-                  ),
-                  onPressed: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const ResultPage())),
-                  child: Text(
-                    '採点する',
-                    style: bodyBold(whitePrimary),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 80),
-        ],
+                const Spacer(),
+                _areFieldsEmpty.value
+                    ? const DisableButton(
+                        text: '採点する',
+                        width: 150,
+                        height: 64,
+                      )
+                    : PrimaryColorButton(
+                        width: 150,
+                        height: 64,
+                        text: '採点する',
+                        onPressed: () async {
+                          //TODO ちょっと長いからまとめる
+                          for (Sentence sentence
+                              in sentenceListNotifier.sentenceList) {
+                            if (sentence.properNoun == controller.text) {
+                              qText = sentence.properNoun;
+                            }
+                          }
+                          if (qText == '') {
+                            const snackBar = SnackBar(
+                              content: Text('ハズレです'),
+                              duration: Duration(seconds: 1),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                            controller.clear();
+                          } else {
+                            ref
+                                .read(sentenceListNotifierProvider.notifier)
+                                .changeSentenceToCollected(
+                                    noun: controller.text);
+                            final snackBar = SnackBar(
+                              backgroundColor: Colors.green,
+                              content: Text(
+                                'あたり！',
+                                style: bodyRegular(whitePrimary),
+                              ),
+                              duration: const Duration(seconds: 1),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                            controller.clear();
+                          }
+                        },
+                      ),
+              ],
+            ),
+            const SizedBox(height: 96),
+          ],
+        ),
       ),
     );
   }
