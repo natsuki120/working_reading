1844a56fee6a331f985b78ff293941a5e693802a
natsuki <synchro0718@icloud.com>
2023-04-14 19:24:04 +0900

    add: 声量を処理するためにnoise_meterパッケージを追加。

--- lib/domain/voice_input/voice_input_notifier.dart
+++ lib/domain/voice_input/voice_input_notifier.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:noise_meter/noise_meter.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:working_reading/domain/sentence/sentence.dart';
@@ -8,25 +11,47 @@ class VoiceInputNotifier extends StateNotifier<VoiceInput> {
   VoiceInputNotifier() : super(const VoiceInput()) {
     initSpeech();
   }
+
   final SpeechToText _speechToText = SpeechToText();
+  StreamSubscription<NoiseReading>? noiseSubscription;
+  final NoiseMeter noiseMeter = NoiseMeter();
 
   void initSpeech() async {
-    state = state.copyWith(
-        speechEnabled: await _speechToText.initialize(),
-        hasSpeechEnough: false);
+    state = state.copyWith(speechEnabled: await _speechToText.initialize());
     startListening();
   }
 
   void startListening() async {
-    await _speechToText.listen(onResult: onSpeechResult, localeId: 'ja-JP');
+    try {
+      await _speechToText.listen(onResult: onSpeechResult, localeId: 'ja-JP');
+      noiseSubscription = noiseMeter.noiseStream.listen(onData);
+      state = state.copyWith(hasSpeechEnough: false);
+    } catch (e) {
+      print(e);
+    }
   }
 
   void stopListening() async {
     await _speechToText.stop();
+    try {
+      if (noiseSubscription != null) {
+        noiseSubscription!.cancel();
+        noiseSubscription = null;
+      }
+      state = state.copyWith(speechEnabled: false);
+    } catch (err) {
+      print('stopRecorder error: $err');
+    }
   }
 
   void onSpeechResult(SpeechRecognitionResult result) {
     state = state.copyWith(lastWord: result.recognizedWords);
+  }
+
+  void onData(NoiseReading noiseReading) {
+    if (!state.speechEnabled) {
+      state = state.copyWith(speechEnabled: true);
+    }
   }
 
   void makeTappableNextButtonIfSpeechEnoughThan(
@@ -50,6 +75,12 @@ class VoiceInputNotifier extends StateNotifier<VoiceInput> {
 
   void resetHasSpeechEnoughValue() {
     state = state.copyWith(hasSpeechEnough: false);
+  }
+
+  void getVoiceIndicatorValue() {
+    noiseMeter.noiseStream.listen((NoiseReading noiseReading) {
+      state = state.copyWith(voiceIndicatorValue: noiseReading.meanDecibel);
+    });
   }
 }
 
