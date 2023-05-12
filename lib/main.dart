1a05b38cdf304a306dbc9bb3994529115683fed3
natsuki <synchro0718@icloud.com>
2023-05-12 21:59:37 +0900

    add: error_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'feature/error/error_page.dart';
 import 'feature/top/top_page.dart';
 import 'firebase_options.dart';
 
@@ -23,11 +24,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(useMaterial3: true),
-        home: const TopPage(),
-        builder: EasyLoading.init());
+    return ScreenUtilInit(
+        designSize: const Size(390, 844),
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(useMaterial3: true),
+            builder: (BuildContext context, Widget? widget) {
+              ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
+                return const ErrorPage(errorMessage: 'errorDetails');
+              };
+              return widget!;
+            },
+            home: TopPage(),
+          );
+        });
   }
 }
